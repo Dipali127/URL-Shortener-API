34ad04511b0f70b52b7ed1c8162fc9d82d453abd
Dipali Bohara <dipalibohara500@gmail.com>
2026-05-12 10:58:13 +0530

    replace local redis with redis cloud configuration

--- redisConfig.js
+++ redisConfig.js
@@ -1,9 +1,20 @@
+// const redis = require('redis');
+// const redisClient = redis.createClient({
+//     url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+// });
+
+
+// module.exports = redisClient;
+
 const redis = require('redis');
+
 const redisClient = redis.createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
 });
 
-
 module.exports = redisClient;
-
-
