89f702c47b7639d90d4c58b5fbfa9f2bca74a86e
Dipali Bohara <dipalibohara500@gmail.com>
2026-05-12 12:48:43 +0530

    add duplicate URL check and custom shortCode feature

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -8,6 +8,8 @@ const redisClient = require('../redisConfig.js');
 const createShortURL = async function (req, res) {
     try {
         const longURL = req.body.longURL;
+        const customShortcode = req.body.customShortcode;
+
         if (!longURL) {
             return res.status(400).send({ status: false, message: " Invalid URL. Please provide a URL." });
 
@@ -17,17 +19,42 @@ const createShortURL = async function (req, res) {
             return res.status(400).send({ status: false, message: " Invalid URL format. Please provide a valid URL " })
         }
 
+        // Check longURL already exist in database
+        const isExistURL = await urlModel.findOne({ longURL: longURL })
+
+        if (isExistURL) {
+            return res.status(200).send({
+                status: true, message: "longURL already exist",
+                data: `${process.env.BASE_URL}/${isExistURL.shortCode}`
+            });
+        }
+
+        if (customShortcode && !isValidShortCode(customShortcode)) {
+            return res.status(400).send({ status: false, message: "Invalid custom shortCode format." });
+        }
+
+        // Check if customShortcode already exists
+        if (customShortcode) {
+            const isExistCode = await urlModel.findOne({ shortCode: customShortcode });
+            if (isExistCode) {
+                return res.status(400).send({
+                    status: false, message: "Custom shortCode already exists. Please use different one."
+                });
+            }
+        }
+
         // Generate shortCode of size 8 by using base 62 characters(0-9,A-Z,a-z)
         const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
         const nanoid = customAlphabet(alphabet, 8)
-        const shortCode = nanoid()
+        const generateShortCode = nanoid()
 
+        const newShortCode = customShortcode ? customShortcode : generateShortCode;
         // Append baseURL with the unique generated shortCode to generate shortURL
-        const shortURL = `${process.env.BASE_URL}/${shortCode}`;
+        const shortURL = `${process.env.BASE_URL}/${newShortCode}`;
 
         const newURL = {
             longURL: longURL,
-            shortCode: shortCode,
+            shortCode: newShortCode,
             urlClickcount: 0
         };
 
@@ -48,8 +75,8 @@ const redirectUrl = async function (req, res) {
 
         if (!isValidShortCode(shortCode)) {
             return res.status(400).send({
-                status: false, message:
-                    " Invalid shortURL format. Please provide a valid shortURL "
+                status: false,
+                message: " Invalid shortURL format. Please provide a valid shortURL "
             });
         }
 
@@ -73,8 +100,6 @@ const redirectUrl = async function (req, res) {
             { new: true }).select('longURL urlClickcount');
 
         if (!isValidshortUrl) {
-            // null is falsy in JavaScript
-            // so !null = true → enters this block
             return res.status(404).send({ status: false, message: "Short URL not found." });
         }
 
@@ -97,50 +122,55 @@ const redirectUrl = async function (req, res) {
 }
 
 
-// Combined click tracker API:
+// click tracker API:
 const handleClick = async function (req, res) {
     try {
         const shortCode = req.params.shortCode;
 
         if (!isValidShortCode(shortCode)) {
             return res.status(400).send({
-                status: false, message: "Invalid shortURL format. Please provide a valid shortURL." });
+                status: false, message: "Invalid shortURL format. Please provide a valid shortURL."
+            });
         }
 
         // Fetch clickCount from Redis database
         const isExistUrl = await redisClient.get(shortCode);
-            if (isExistUrl) {
-                let parseUrl = JSON.parse(isExistUrl);
-                return res.status(200).send({ status: true, message: "total clicks",
-                     totalClicks: parseUrl.urlClickcount })
-            }
+        if (isExistUrl) {
+            let parseUrl = JSON.parse(isExistUrl);
+            return res.status(200).send({
+                status: true, message: "total clicks",
+                totalClicks: parseUrl.urlClickcount
+            })
+        }
 
-            // If not in Redis, fetch from MongoDB
-            const isValidShortUrl = await urlModel.findOne({ shortCode: shortCode })
+        // If not in Redis, fetch from MongoDB
+        const isValidShortUrl = await urlModel.findOne({ shortCode: shortCode })
             .select('longURL urlClickcount');
 
-            if (!isValidShortUrl) {
-                return res.status(404).send({ status: false, message: "Short URL not found." });
-            }
-
-            // Store in redis database for future access
-            const redisData = {
-                longURL: isValidShortUrl.longURL,
-                urlClickcount: isValidShortUrl.urlClickcount
-            };
-            
-            await redisClient.set(shortCode, JSON.stringify(redisData),{
-                EX:3600
-            });
-
-
-            return res.status(200).send({ status: true, message: "total clicks", totalClicks:
-                 isValidShortUrl.urlClickcount })
-
-        } catch (error) {
-            return res.status(500).send({ status: false, message: error.message });
+        if (!isValidShortUrl) {
+            return res.status(404).send({ status: false, message: "Short URL not found." });
         }
+
+        // Store in redis database for future access
+        const redisData = {
+            longURL: isValidShortUrl.longURL,
+            urlClickcount: isValidShortUrl.urlClickcount
+        };
+
+        await redisClient.set(shortCode, JSON.stringify(redisData), {
+            EX: 3600
+        });
+
+
+        return res.status(200).send({
+            status: true, message: "total clicks", totalClicks:
+                isValidShortUrl.urlClickcount
+        })
+
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
     }
+}
 
 module.exports = { createShortURL, redirectUrl, handleClick }
 
--- validator/validation.js
+++ validator/validation.js
@@ -1,9 +1,11 @@
 // validation.js
 
 // Validates whether the provided URL string is properly structured.
-// The URL must include a valid protocol (http, https, or ftp) followed by a valid hostname or domain name.
-// This function uses Node.js's built-in URL parser, which ensures that the overall URL format 
-// (including optional paths, query parameters, and fragments) is valid.
+// The URL must include a valid protocol (http, https) 
+// followed by a valid hostname or domain name.
+// This function uses Node.js's built-in URL parser, which ensures 
+// that the overall URL format (including optional paths, query parameters, 
+// and fragments) is valid.
 
 const isValidUrl = (url) => {
     try {
@@ -16,11 +18,11 @@ const isValidUrl = (url) => {
 
 // Validates whether the provided shortCode is in the correct format.
 // The shortCode should contain only alphanumeric characters (letters and digits).
+// The shortCode must be minimum 4 and maximum 8 characters long.
 // Returns true if the shortCode is valid otherwise, returns false.
 
 const isValidShortCode = (shortcode) => {
-    return /^[a-zA-Z0-9]+$/.test(shortcode);
+    return /^[a-zA-Z0-9]{4,8}$/.test(shortcode);
 }
-
 
 module.exports = { isValidUrl, isValidShortCode };
